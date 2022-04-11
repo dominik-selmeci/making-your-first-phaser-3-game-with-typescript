c7a6f7be84eb51b85c40c60ee510e2a4ac55a88d
Dominik Šelmeci <dominik.selmeci@alza.cz>
2022-04-11 21:40:29 +0200

    Part 5 - The Player

--- src/scenes/PhaserScene.ts
+++ src/scenes/PhaserScene.ts
@@ -1,33 +1,62 @@
-import { Scene, Physics } from 'phaser';
+import { Scene, Physics } from "phaser";
 
 export default class HelloWorldScene extends Scene {
   private platforms: Physics.Arcade.StaticGroup | null = null;
 
   preload() {
     // image size is taken from file
-    this.load.image('sky', 'assets/sky.png');
-    this.load.image('ground', 'assets/platform.png');
-    this.load.image('star', 'assets/star.png');
-    this.load.image('bomb', 'assets/bomb.png');
-    this.load.spritesheet('dude', 'assets/dude.png', { frameWidth: 32, frameHeight: 48 });
+    this.load.image("sky", "assets/sky.png");
+    this.load.image("ground", "assets/platform.png");
+    this.load.image("star", "assets/star.png");
+    this.load.image("bomb", "assets/bomb.png");
+    this.load.spritesheet("dude", "assets/dude.png", { frameWidth: 32, frameHeight: 48 });
   }
 
   create() {
     // x,y - images are centered in the middle
-    this.add.image(400, 300, 'sky');
+    this.add.image(400, 300, "sky");
 
     // or you can set origin
     // this.add.image(0, 0, 'sky').setOrigin(0, 0);
     // the order of the images are important
 
-    // this.physics is not initialized in the constructor and is for arcade physics
-    this.platforms = this.physics.add.staticGroup();
-    this.platforms.create(400, 568, 'ground').setScale(2).refreshBody();
-
-    this.platforms.create(600, 400, 'ground');
-    this.platforms.create(50, 250, 'ground');
-    this.platforms.create(750, 220, 'ground');
+    this.createPlatforms();
+    this.createPlayer();
   }
 
   update() {}
+
+  private createPlatforms() {
+    // this.physics is not initialized in the constructor and is for arcade physics
+    this.platforms = this.physics.add.staticGroup();
+    this.platforms.create(400, 568, "ground").setScale(2).refreshBody();
+
+    this.platforms.create(600, 400, "ground");
+    this.platforms.create(50, 250, "ground");
+    this.platforms.create(750, 220, "ground");
+  }
+
+  private createPlayer() {
+    this.physics.add.sprite(100, 450, "dude").setBounce(0.2).setCollideWorldBounds(true);
+
+    this.anims.create({
+      key: "left",
+      frames: this.anims.generateFrameNumbers("dude", { start: 0, end: 3 }),
+      frameRate: 10,
+      repeat: -1,
+    });
+
+    this.anims.create({
+      key: "turn",
+      frames: [{ key: "dude", frame: 4 }],
+      frameRate: 20,
+    });
+
+    this.anims.create({
+      key: "right",
+      frames: this.anims.generateFrameNumbers("dude", { start: 5, end: 8 }),
+      frameRate: 10,
+      repeat: -1,
+    });
+  }
 }
